c63c32a4ccaaaa3e94ce0e53918eb3629f0893fe
buggyminer <liaojiudazhongbiao@outlook.com>
2021-06-22 16:24:30 +0800

    initialize graph_coloring

--- grap_coloring.py
+++
@@ -1,5 +0,0 @@
-import random
-import time
-import unittest
-
-def generate_graph():
---
+++ jwy/graph_coloring.py
@@ -0,0 +1,20 @@
+import random
+import time
+import unittest
+
+def generate_graph(n):
+    vector=[[0]*n for i in range(n)]
+
+
+def graph_coloring_greedy(vector,colors):
+    pass
+
+def graph_coloring_recall(vector,colors):
+    pass
+
+class grapg_coloring_test(unittest.TestCase):
+    def test_coloring_result(self):
+        pass
+
+    def compare_coloring_speed(self):
+        pass
