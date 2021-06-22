748addfda80349b67a88aafab3a0fd37e7552b6a
buggyminer <liaojiudazhongbiao@outlook.com>
2021-06-22 16:18:02 +0800

    add pseudocode

--- jwy/coin_collection.py
+++ jwy/coin_collection.py
@@ -2,30 +2,39 @@ import random
 import time
 import unittest
 
+"""
+1、根据参数初始化二维列表
+2、根根据网格大小生成dp表
+3、根据状态方程，逐行完成dp表
+4、返回终点的dp表值
+"""
 
+
+# 初始化网格
 def generate_table(n, m, p, v):
     table = [[0] * m for i in range(n)]
+    # 更新随机数种子
     random.seed(time.process_time())
     count = 0
     while count < p:
         i = random.randint(0, n * m - 1)
         x = i // m
         y = i % m
-        # print("x is ", x, "y is", y)
         if(table[x][y]==0):
             count=count+1
             table[x][y] = random.randint(1, v)
     return table
 
-
+# 使用dp解决问题
 def dp(table):
+    # 初始化dp列表
     n, m = len(table), len(table[0])
     dp = [[0] * (m + 1) for i in range(n + 1)]
-
+# 编写撞状态方程
     for i in range(n):
         for j in range(m):
             dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]) + table[i][j]
-
+# 返回dp表末尾的值
     return dp[n][m]
 
 
@@ -38,6 +47,7 @@ if __name__ == '__main__':
 
 
 class coin_collection_test(unittest.TestCase):
+    # 测试参数是否正确
     def test_generate_coin(self):
         n, m, p, v = 10, 7, 40, 30
         table = generate_table(n, m, p, v)
@@ -52,6 +62,7 @@ class coin_collection_test(unittest.TestCase):
         print(count)
         self.assertEqual(count, p)
 
+    # 手工编写测试用例
     def test_dp_right(self):
         # table = generate_table(10, 7, 40, 30)
         test_table_list=[
