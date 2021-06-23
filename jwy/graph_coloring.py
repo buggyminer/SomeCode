b0555c6877e6f89d1ab0723cbc0fef1efca80262
buggyminer <liaojiudazhongbiao@outlook.com>
2021-06-24 07:46:10 +0800

    add recall function

--- jwy/graph_coloring.py
+++ jwy/graph_coloring.py
@@ -23,15 +23,15 @@ def color_decode(color):
 def plot_graph(vector, colors=None):
     if (colors == None):
         colors = [0 for i in range(len(vector))]
-    edges=[]
+    edges = []
     G = nx.Graph()
     for row, i in zip(vector, range(len(vector))):
         for col, j in zip(row, range(len(row))):
             if (col != 0):
                 G.add_edge(i, j)
-    nodes=list(zip(list(G.nodes),colors))
-    sorted_list=sorted(nodes,key=lambda x:x[0])
-    colors=[x[1] for x in sorted_list]
+    nodes = list(zip(list(G.nodes), colors))
+    sorted_list = sorted(nodes, key=lambda x: x[0])
+    colors = [x[1] for x in sorted_list]
     nx.draw(G, node_color=[color_decode(color) for color in colors], with_labels=True)
     plt.show()
 
@@ -48,14 +48,36 @@ def graph_coloring_greedy(vector, color_num):
                 if (color > color_num):
                     return None
                 if (check_if_color_satisfied(adjacents, colors, color)):
-                    colors[i]=color
+                    colors[i] = color
                     break
     return colors
 
 
 # 回溯法寻找最优解
-def graph_coloring_recall(vector, colors):
-    pass
+def graph_coloring_recall(vector, color_num):
+    stack = []
+    visit = [0 for i in range(len(vector))]
+    colors = [0 for i in range(len(vector))]
+
+    stack.append(0)
+    visit[0] = 1
+    while (len(stack) > 0):
+        node_now = stack.pop()
+        visit[node_now] = 1
+        while colors[node_now] < color_num:
+            colors[node_now] = colors[node_now] + 1
+            if check_if_color_satisfied(vector[node_now], colors, colors[node_now]):
+                break
+        else:
+            if 0 in visit:
+                colors[node_now] = 0
+                visit[node_now] = 0
+                continue
+        if 0 not in visit:
+            return colors
+        for i in range(len(vector[node_now])):
+            if vector[node_now][i] == 1 and visit[i] == 0:
+                stack.append(i)
 
 
 # if __name__ == '__main__':
@@ -94,13 +116,13 @@ class grapg_coloring_test(unittest.TestCase):
         {
             "vector":
                 [
-                    [0, 1, 1, 0, 1],
-                    [1, 0, 0, 1, 0],
-                    [1, 0, 0, 1, 0],
                     [0, 1, 1, 0, 0],
-                    [1, 0, 0, 0, 0],
+                    [1, 0, 1, 0, 0],
+                    [1, 1, 0, 1, 1],
+                    [0, 0, 1, 0, 1],
+                    [0, 0, 1, 1, 0],
                 ],
-            "color_num": 2
+            "color_num": 3
         }
     ]
 
@@ -122,6 +144,16 @@ class grapg_coloring_test(unittest.TestCase):
                 plot_graph(vector, colors)
         self.assertTrue(True)
 
+    def test_graph_coloring_recall(self):
+        for graph in self.test_graph_list:
+            vector = graph["vector"]
+            color_num = graph["color_num"]
+            colors = graph_coloring_recall(vector, color_num)
+            if colors:
+                if not self.__coloring_result_test(vector, colors):
+                    self.assertTrue(False)
+                plot_graph(vector, colors)
+        self.assertTrue(True)
 
     def test_compare_coloring_speed(self):
         self.assertTrue(True)
